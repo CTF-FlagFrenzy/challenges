1676a29fc25cd008609950ef270e87148b644e24
zVSciy <manager.fallen0815@gmail.com>
2024-10-03 12:19:16 +0200

    Add Solana Assets challenge documentation files

---
+++ Solana_Assets/Solana_Assets/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Solana_Assets_App.urls')),  # Ändern Sie dies, um die App-URLs in der Root-URL zu haben
+]
