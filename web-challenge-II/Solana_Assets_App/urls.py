240358ddcdfd6104ed9568904267aea3bdb5c443
zVSciy <manager.fallen0815@gmail.com>
2024-10-21 08:34:33 +0200

    create initial sites for web challenge 2

---
+++ web-challenge-II/Solana_Assets_App/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('news/', views.news, name='news'),
+    path('login/', views.login_view, name='login'),
+    path('right/', views.wrong, name='wrong'),
+    path('files/', views.files, name='files'),
+]
