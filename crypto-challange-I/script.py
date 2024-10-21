c5e9ae7d932dab48112201a7e1838083991a481b
KaveXD <KaveXD@gmail.com>
2024-10-21 08:43:48 +0000

    Update Dockerfile, docker-compose.yml, startup script, and Python script for crypto-challenge-I

--- crypto-challange-I/script.py
+++ crypto-challange-I/script.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+teamkey = os.getenv("TEAMKEY")
+challangeKey = os.getenv("CHALLANGEKEY")
+print(f"Team key: {teamkey}")
+print(f"Challange key: {challangeKey}")
 
 print("Running startup script...")
 # Add your startup logic here
--- crypto-challange-I/start.sh
+++ crypto-challange-I/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+
+# apt-get update && apt-get install -y python3-pip
+
 # Run the Python script
+python3 -m pip install requirements.txt
 python3 /usr/local/bin/script.py
 
 # Start Apache server
