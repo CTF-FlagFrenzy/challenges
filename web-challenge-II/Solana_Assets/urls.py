39083f952d04a67a7c698dbf810f4e6f2a1f0519
zVSciy <88750044+zVSciy@users.noreply.github.com>
2024-10-23 13:42:36 +0200

    Remove unnecessary files and code related to Solana Assets web challenge

--- web-challenge-II/Solana_Assets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Solana_Assets_App.urls')),  # Ändern Sie dies, um die App-URLs in der Root-URL zu haben
-]
