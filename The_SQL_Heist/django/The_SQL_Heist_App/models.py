2629ceb186afce7366dc98c3e3d04e73d35977c4
zVSciy <88750044+zVSciy@users.noreply.github.com>
2024-10-24 20:19:10 +0200

    Added new models and refactored views.py. The user is now able to use the search feature to retrieve the flag through a SQL injection vulnerability in the Django built-in database.

--- The_SQL_Heist/django/The_SQL_Heist_App/models.py
+++ The_SQL_Heist/django/The_SQL_Heist_App/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- The_SQL_Heist/django/The_SQL_Heist_App/views.py
+++ The_SQL_Heist/django/The_SQL_Heist_App/views.py
@@ -1,10 +1,39 @@
 import logging
-
+from django.db import connection
+from .models import Article
 from django.shortcuts import render
+import hashlib
+import random
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
-
 def index(request):
-    response = render(request, "SQL_Heist/index.html")
-    return response
+    query = request.GET.get('q')
+    #! This is the flag that you need to get from the environment variables
+    # teamflag = os.environ.get('TEAMFLAG')
+    flag = None
+    teamflag = "teamflag"
+    challengeflag = "nkN7FPpuB#"
+    combined_flag = challengeflag + teamflag
+    hashed_flag = hashlib.sha256(combined_flag.encode()).hexdigest()
+
+    articles = []
+    if query:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM The_SQL_Heist_App_article WHERE title LIKE '%{query}%' OR content LIKE '%{query}%'")
+            rows = cursor.fetchall()
+            for row in rows:
+                articles.append({
+                    'title': row[1],  # Assuming title is the second column
+                    'content': row[2]  # Assuming content is the third column
+                })
+            # Überprüfen Sie, ob die SQL-Injection erfolgreich war
+            if "flag" in query:
+                flag = f"FF{{{hashed_flag}}}"
+    else:
+        articles = Article.objects.all()
+
+    # Get 5 random articles for breaking news
+    breaking_news = random.sample(list(Article.objects.all()), 5)
+
+    return render(request, 'SQL_Heist/index.html', {'articles': articles, 'flag': flag, 'breaking_news': breaking_news})
