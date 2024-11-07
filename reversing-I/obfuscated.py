3dacc6f6adc48271ac274312cdc4b58c7a753544
KaveXD <KaveXD@gmail.com>
2024-11-07 11:15:10 +0000

    Refactor reversing-I directory structure and remove unnecessary files

---
+++ reversing-I/obfuscated.py
@@ -0,0 +1,8 @@
+
+    import base64
+    import random
+    import os
+    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+    from cryptography.hazmat.primitives import padding
+    from cryptography.hazmat.backends import default_backend
+    
