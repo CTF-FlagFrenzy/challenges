37984be66f0f0d06c00ba131666b760bded42970
KaveXD <KaveXD@gmail.com>
2024-12-19 08:15:34 +0000

    Enhance assembly program to retrieve and print environment variable; update Dockerfile and installation script for necessary dependencies

--- Assembly_Container/Alpine/Assembly/hello.asm
+++ Assembly_Container/Alpine/Assembly/hello.asm
@@ -1,23 +1,54 @@
-;---------------------
-;  Flat Assembler file
-;  Syscall Hello World
-;---------------------
 section .data
-msg db 'Hello 64-bit world!', 0xA
-msg_size equ $ - msg
+    env_var db 'HOME', 0  ; Environment variable name
+    newline db 0xA
 
 section .text
-global _start
+
+    extern getenv
+    extern puts
+    extern exit
+    global _start
 
 _start:
-  ; sys_write
-  mov edx, msg_size
-  lea rsi, [msg]
-  mov edi, 1
-  mov eax, 1
-  syscall
+    ; Save the current base pointer
+    push rbp
 
-  ; sys_exit
-  mov eax, 60
-  xor edi, edi
-  syscall
+    ; Set the new base pointer
+    mov rbp, rsp
+
+    ; Allocate space for the environment variable name
+    sub rsp, 8
+
+    ; Copy the environment variable name to the stack
+    lea rdi, [env_var]
+    call getenv
+
+    ; Restore the stack
+    add rsp, 8
+
+    ; Save the result in a register
+    mov rbx, rax
+
+    ; Check if the environment variable was found
+    test rax, rax
+    jz env_var_not_found
+
+    ; Print the environment variable value
+    mov rdi, rax
+    call puts
+
+    ; Exit the program
+    mov edi, 0
+    call exit
+
+env_var_not_found:
+    ; Print an error message
+    lea rdi, [error_msg]
+    call puts
+
+    ; Exit the program with an error code
+    mov edi, 1
+    call exit
+
+section .data
+    error_msg db 'Environment variable not found', 0
--- Assembly_Container/Alpine/installation.sh
+++ Assembly_Container/Alpine/installation.sh
@@ -1,2 +1,4 @@
 apk add nasm
 apk add binutils
+apk add gcc
+apk add libc-dev
