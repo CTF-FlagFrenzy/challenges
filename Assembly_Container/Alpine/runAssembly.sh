a630db6404f4a708924fcf8fa8e3aac280a45d48
KaveXD <KaveXD@gmail.com>
2025-01-16 10:01:41 +0000

    Refactor Docker setup; change base image, update exposed ports, and add entrypoint script for assembly execution

---
+++ Assembly_Container/Alpine/runAssembly.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "ALERT"
+>&2 echo "ALERT"
+touch /app/successfull
+
+cd /uploads
+# compile
+nasm -f elf64 assembly-script.asm -o script.o
+# link
+ld script.o -o script -dynamic-linker /lib/ld-musl-x86_64.so.1 -lc
+# run
+./script
