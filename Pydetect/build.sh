d74bece0dec25604ee870560aa07c607308625c3
cupels <108838326+cupels@users.noreply.github.com>
2025-01-30 16:42:54 +0100

    update build process

---
+++ Pydetect/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python3 -m pip install --no-cache-dir -r requirements.txt
+
+apt update
+apt install zip -y
--- Pydetect/entry.sh
+++ Pydetect/entry.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-python3 -m pip install --no-cache-dir -r requirements.txt
+
 python3 main.py
 
-apt update
-apt install zip -y
 
 zip solveme.zip encoder.pyc flag
 mv solveme.zip ./webroot/solveme.zip
