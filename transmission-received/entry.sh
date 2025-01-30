567c5ef2746ee97930843a7a5c071f9861b8b363
cupels <108838326+cupels@users.noreply.github.com>
2025-01-30 15:58:55 +0100

    update build process

--- transmission-received/entry.sh
+++ transmission-received/entry.sh
@@ -1,8 +1,8 @@
-#/usr/bin/sh
+#!/usr/bin/sh
 
-python3 -m pip install -r requirements.txt
 python3 main.py
-python3 -m pysstv --mode Robot36 placed.png ./webroot/transmission.wav
+python3 -m pysstv --mode Robot36 placed.png ./transmission.wav
+ffmpeg -i transmission.wav -filter:a "volume=0.1" ./webroot/transmission.wav
 
 cd webroot/
 python3 -m http.server 80
