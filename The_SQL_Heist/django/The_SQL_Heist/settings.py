21e7cfe3d00cbd609c1ce8b9d02c07f59053f114
zVSciy <manager.fallen0815@gmail.com>
2025-02-05 14:48:52 +0100

    Update CSRF settings and secure cookie configurations in Django settings

--- The_SQL_Heist/django/The_SQL_Heist/settings.py
+++ The_SQL_Heist/django/The_SQL_Heist/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_URL = "index"
@@ -50,8 +46,9 @@ SECRET_KEY = "django-insecure-+r4^t++!&gsoz!pw28pke4a^emiw228ne4@cd3&i3hzk8gjrf-
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*.web.ctf.htl-villach.at"]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 
 # Application definition
