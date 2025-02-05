0d1c812fb15e3a99b51f2214226d2f6c02381e07
zVSciy <manager.fallen0815@gmail.com>
2025-02-05 13:44:27 +0100

    Add Capture the Bytes challenge with Flask app and PCAP generation

---
+++ Capture_the_Bytes/flask_app/app.py
@@ -0,0 +1,20 @@
+import subprocess
+
+from flask import Flask, render_template, send_from_directory, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    subprocess.run(["python", "flask_app/create_pcap.py"])
+    return redirect("/capture.pcap")
+
+
+@app.route("/capture.pcap")
+def capture_file():
+    import os
+    return send_from_directory(os.path.join(app.root_path, "capture"), "capture.pcap")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
