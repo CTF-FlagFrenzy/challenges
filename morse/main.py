d236a79a868a1c7b9b05c0eefc44118976f8f7b7
cupels <108838326+cupels@users.noreply.github.com>
2025-02-20 12:14:21 +0100

    change morse audio file name, new decoy message and other small fixes

--- morse/main.py
+++ morse/main.py
@@ -6,11 +6,7 @@ challengekey = os.getenv('CHALLANGEKEY')
 teamkey = os.getenv('TEAMKEY')
 
 flaghash = hashlib.sha256(('%s%s' % (teamkey, challengekey)).encode()).hexdigest()
-print (flaghash)
 flag = 'FF{%s}' % flaghash
 
 pycw.output_wave('tones.wav', flaghash, 24, 20000)
-#pycw.output_wave('base.wav', 'The signal you seek is beyond what ears perceive. Only the keenest analysis will reveal the truth.', 24, 500)
-
-
-
+#pycw.output_wave('base.wav', 'Some truths whisper, barely a breath above silence. Only those who listen beyond the range of hearing will find the key.', 24, 500)
