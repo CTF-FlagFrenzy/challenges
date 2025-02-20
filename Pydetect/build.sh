2b9012adc969ccee9286997bf0618c4fe17446dc
cupels <108838326+cupels@users.noreply.github.com>
2025-02-20 09:23:42 +0100

    update flag generation

--- Pydetect/build.sh
+++ Pydetect/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-python3 -m pip install --no-cache-dir -r requirements.txt
-
 apt update
 apt install zip -y
--- Pydetect/main.py
+++ Pydetect/main.py
@@ -1,10 +1,10 @@
-import hashlib, os, zbase32, zlib, base64, pickle
+import hashlib, os, zlib, base64, pickle
 
 teamkey = os.getenv("TEAMKEY")
 challenge = os.getenv("CHALLENGEKEY")
 
 flag = "%s%s" % (teamkey, challenge)
-flaghash = zbase32.encode(hashlib.sha256(flag.encode()).digest())
+flaghash = (hashlib.sha256(flag.encode()).hexdigest())
 hashed_flag = "FF{%s}" % flaghash
 
 enc = base64.urlsafe_b64encode(zlib.compress(bytes(hashed_flag, 'utf-8')))
