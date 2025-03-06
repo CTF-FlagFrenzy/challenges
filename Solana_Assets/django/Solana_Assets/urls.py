b1db99487ab46b8d6dc366af5c469a7f836abb20
zVSciy <manager.fallen0815@gmail.com>
2025-03-06 12:12:30 +0100

    Remove admin URL path from main URL configuration

--- Solana_Assets/django/Solana_Assets/urls.py
+++ Solana_Assets/django/Solana_Assets/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path(
         "", include("Solana_Assets_App.urls")
     ),  # Ändern Sie dies, um die App-URLs in der Root-URL zu haben
